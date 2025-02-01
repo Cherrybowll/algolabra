172702bf5dd6ef6d81b70bf4f14800a3696a65e5
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-02-01 23:22:11 +0200

    Fix wrongly used abs function in extended euclidean and add failsafe

--- src/utilities/extended_euclidean.py
+++ src/utilities/extended_euclidean.py
@@ -1,4 +1,9 @@
 def extended_euclidean(a : int, b: int):
+
+    #Failsafe for the edge case gcd(0, 0), which isn't properly defined
+    if a == 0 and b == 0:
+        raise ValueError("Greatest common divisor is not defined for two zeros.")
+
     old_x, x = 1, 0
     old_y, y = 0, 1
 
@@ -6,10 +11,10 @@ def extended_euclidean(a : int, b: int):
         quotient = a // b
         a, b = b, a % b
         old_x, x = x, old_x - quotient * x
-        old_y, y = y, old_y - quotient * x
+        old_y, y = y, old_y - quotient * y
     
     return {
         "gcd": abs(a),
-        "coefficients": (abs(old_x), abs(old_y)),
+        "coefficients": (old_x, old_y),
         "quotients": (abs(y), abs(x))
     }
