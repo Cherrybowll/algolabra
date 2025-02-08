fae54f0be01a8b964ef30576ebd6c3687f8029d9
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-02-08 23:45:34 +0200

    Update main script

--- src/index.py
+++ src/index.py
@@ -1,7 +1,9 @@
 from utilities.rsa import RSA
+from utilities.filehelper import FileHelper
 from ui import UI
 
 rsa_handler = RSA()
-ui = UI(rsa_handler)
+file_handler = FileHelper()
+ui = UI(rsa_handler, file_handler)
 
 ui.start()
