9b89483d9f0fc3d5413109b046928adacd1add92
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2025-02-22 13:25:56 +0200

    Add docstring for RSA

--- src/utilities/rsa.py
+++ src/utilities/rsa.py
@@ -7,25 +7,65 @@ import secrets
 
 
 class RSA:
+    """Class that implements the RSA algorithm, including key generation, encryption and decryption.
+    """
+
     def __init__(self):
         pass
 
     def generate_keys(self):
+        """Generates a public-private RSA key.
+
+        Returns:
+            dict: Parts of the key with product of primes as 'n',
+            public exponent as 'e' and private exponent as 'd'.
+        """
+
         p, q = self._find_primes()
         n = p * q
         n_totient = abs((p-1)*(q-1)) // euclidean(p-1, q-1)
+
+        # Public exponent
         e = 65537
+
+        #Private exponent
         d = extended_euclidean(e, n_totient)["coefficients"][0]
 
         return {"n": n, "e": e, "d": d}
 
     def encrypt(self, message, key):
+        """Encrypts/signs a message with the selected key.
+
+        Args:
+            message (int): Integer representation of the message to be encrypted.
+            key (dict): Dictionary with keys 'n' and 'exponent'.
+
+        Returns:
+            int: The encrypted/signed message.
+        """
+
         return pow(message, key["exponent"], key["n"])
 
     def decrypt(self, cipher, key):
+        """Decrypts a message with the selected key.
+
+        Args:
+            cipher (int): The integer cipher of the original message.
+            key (dict): Dictionary with keys 'n' and 'exponent'.
+
+        Returns:
+            int: The decrypted message.
+        """
+
         return pow(cipher, key["exponent"], key["n"])
 
     def _find_primes(self):
+        """Private method for finding large prime numbers for key generation.
+
+        Returns:
+            tuple: A tuple with two large prime numbers.
+        """
+
         small_primes = sieve_of_eratosthenes(5000)
         found_primes = []
 
@@ -53,6 +93,16 @@ class RSA:
         return found_primes
 
     def _check_suitable_totient(self, totient: int, e: int):
+        """Private method for checking that the totient of n is coprime with the public exponent e.
+
+        Args:
+            totient (int): Totient of n, the product of the large primes used for key generation.
+            e (int): The public exponent of the key.
+
+        Returns:
+            bool: Whether the check passes.
+        """
+
         return euclidean(totient, e) == 1
 
     def _check_primes_sufficient_distance(self, p: int, q: int):
